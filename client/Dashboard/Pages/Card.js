14cccf8bbf84cda2fbd0c0b0d4b3aa54d5bac17f
MaximeParisi <144666976+MaximeParisi@users.noreply.github.com>
2024-03-04 10:40:27 +0100

    add style on card

--- client/Dashboard/Pages/Card.js
+++ client/Dashboard/Pages/Card.js
@@ -5,9 +5,42 @@ import { View, Text, StyleSheet, TouchableOpacity, TextInput } from 'react-nativ
 let titleCard = "Card"
 const Card = () => {
   return (
-    <View>
-
+    <View sttyle= {styles.CardPage}>
+        <View style= {styles.CardBody}>
+            <Text style = {componentStyle.text}>{titleCard}</Text>
+            <TouchableOpacity
+                onpressed = {console.log("affichage menu action pour une card")}
+            >
+                <Text style = {componentStyle.text}>...</Text>
+            </TouchableOpacity>           
+        </View>
     </View>
   )}
+
+const styles = StyleSheet.create({
+    CardPage : {
+        backgroundColor: "#ffffff",
+        width: 265,
+        height: 50,
+        borderRadius: 20,
+        margin: 20
+    },
+    CardBody: {
+        backgroundColor: "grey",
+        flexDirection: "row",
+        justifyContent: "space-between",
+        alignItems: "center",
+        padding: 30,
+        borderTopLeftRadius: 20,
+        borderTopRightRadius: 20,
+    },
+        
+})
+
+const componentStyle = StyleSheet.create({
+    text : {
+        color: "#A1B5FE"
+    }
+})
 
 export default Card;
