1093122cd6b4a5d868cf8825cb14d649314be3a5
chrisDev06 <christophermassondev.epitech@gmail.com>
2024-03-05 15:21:40 +0100

    front affiche les users ou une img

--- client/Dashboard/Component/navBar.js
+++ client/Dashboard/Component/navBar.js
@@ -1,9 +1,21 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, TouchableOpacity, StyleSheet, Image, Modal, Text, Button } from 'react-native';
 import { FontAwesome } from '@expo/vector-icons';
+import axios from 'axios';
 
 const NavBar = ({ openNav, isNavOpen }) => {
   const [modalVisible, setModalVisible] = useState(false);
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get('url_de_votre_api')
+      .then(response => {
+        setUsers(response.data);
+      })
+      .catch(error => {
+        console.error('Error fetching users:', error);
+      });
+  }, []);
 
   const openProfile = () => {
     setModalVisible(true);
@@ -12,6 +24,7 @@ const NavBar = ({ openNav, isNavOpen }) => {
   const closeModal = () => {
     setModalVisible(false);
   };
+
   return (
     <View style={styles.container}>
       <View style={styles.leftContainer}>
@@ -21,14 +34,25 @@ const NavBar = ({ openNav, isNavOpen }) => {
       </View>
 
       <View style={styles.rightContainer}>
-        <TouchableOpacity onPress={openProfile}>
-          <Image
-            source={require('../../assets/user.png')}
-            style={styles.profileImage}
-          />
-
-        </TouchableOpacity>
+        {users.length > 0 ? (
+          users.map(user => (
+            <TouchableOpacity key={user.id} onPress={openProfile}>
+              <Image
+                source={{ uri: user.avatar }}
+                style={styles.profileImage}
+              />
+            </TouchableOpacity>
+          ))
+        ) : (
+          <TouchableOpacity onPress={openProfile}>
+            <Image
+              source={require('../../assets/user.png')}
+              style={styles.profileImage}
+            />
+          </TouchableOpacity>
+        )}
       </View>
+
       <Modal
         animationType="slide"
         transparent={true}
